6e9338db2b0eb40a18f65f668c2b6bdb1c75bb25
Lai Zit Seng <lzs@zitseng.com>
2020-02-01 15:35:11 +0800

    Scrape data through multiple pages

--- ws_clientlist.pl
+++ ws_clientlist.pl
@@ -6,13 +6,13 @@ use File::Temp qw/ :POSIX /;
 
 use Getopt::Long;
 
+sub fetch_pagelis($);
 sub parse_client($);
 
     my $USER;
     my $PASS;
     my $HOST;
     my $url;
-    my $total_entries;
 
     if (GetOptions('user=s' => \$USER,
                    'pass=s' => \$PASS,
@@ -20,30 +20,48 @@ sub parse_client($);
         die;
     }
 
-    my $url = "https://$HOST/screens/apf/mobile_station_list.html?pgInd=1";
+    my $url = "https://$HOST/screens/apf/mobile_station_list.html?pgInd=";
 
     my $cookiefile = tmpnam();
 
     # Login to get session cookie, no need the output
     my $OUT = qx(wget --keep-session-cookies --save-cookies $cookiefile --no-check-certificate --user $USER --password $PASS $url -O - 2> /dev/null);
 
-    $OUT = qx(wget --load-cookies $cookiefile  --no-check-certificate --user $USER --password $PASS $url -O - 2> /dev/null);
+    my $page = 0;
+    my $total_rows_found = 0;
+    my $total_entries = 0;
+    do {
+        my $rows_found;
+
+        $page++;
+        ($rows_found, $total_entries) = fetch_pagelist($page);
+        $total_rows_found += $rows_found;
+    } until ($total_rows_found >= $total_entries);
+
+    unlink $cookiefile;
+
+    exit;
+
+sub fetch_pagelist($){
+    my ($page) = @_;
+    my $rows_found = 0;
+    my $total_entries;
+
+    $OUT = qx(wget --load-cookies $cookiefile  --no-check-certificate --user $USER --password $PASS $url$page -O - 2> /dev/null);
 
     if ($OUT =~ /total_entries" VALUE="(\d+)"/s) {
         $total_entries = $1;
-        print "DEBUG total_entries = $total_entries\n";
     }
 
     while ($OUT =~ /<tr>(.*?)<\/tr>/gs) {
         my $row = $1;
         if ($row =~ /var indexVal =(\d+);/s) {
             parse_client($row);
+            $rows_found++;
         }
     }
-
-    unlink $coookiefile;
-
-    exit;
+    return ($rows_found, $total_entries);
+}
 
 sub parse_client($)
 {
